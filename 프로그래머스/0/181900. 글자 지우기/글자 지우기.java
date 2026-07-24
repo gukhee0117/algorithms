7fd0ee2757705b09f3cac32aeaa942ce017cc5b1
gukhee <gukhee0117@gmail.com>
2026-07-25 00:16:01 +0900

    [level 0] Title: 글자 지우기, Time: 0.06 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,21 +1,24 @@
 import java.util.*;
 
-class Solution {
+
+        class Solution {
     public String solution(String my_string, int[] indices) {
-        String answer = "";
-        HashMap<Integer,Character> map = new HashMap<>();
 
+        boolean[] remove = new boolean[my_string.length()];
 
-        for(int i=0;i<my_string.length();i++){
-            map.put(i,my_string.charAt(i));
+        for (int index : indices) {
+            remove[index] = true;
         }
-        for(int i=0;i<indices.length;i++){
-            map.remove(indices[i]);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (!remove[i]) {
+                sb.append(my_string.charAt(i));
+            }
         }
-        for(char ch:map.values()){
-            answer+=ch;
-        }
-        System.out.println(answer);
-        return answer;
+
+        return sb.toString();
     }
 }
+    
